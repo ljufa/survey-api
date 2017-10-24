2cee238322f560accbf8a2511545f4f3aef533eb
Dragan Ljubojevic <ljufa@yahoo.com>
2017-10-24 21:01:40 +0200

    move to java 9

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 args=("$@")
 if test "$#" -eq 1; then
-    ./mvnw clean package -pl ${args[0]} -DskipTests=true
+    ./mvnw -s settings.xml clean package -pl ${args[0]} -DskipTests=true
     docker-compose up -d --no-deps --build ${args[0]}
 fi
 if test "$#" -eq 2; then
